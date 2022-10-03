b81b3290197025a75c2c7efb41bf6274e38d831a
aaliyan1230 <73843030+aaliyan1230@users.noreply.github.com>
2022-10-03 14:55:03 +0500

    cart reducers added

---
+++ src/state/reducers/index.js
@@ -0,0 +1,12 @@
+import cartActiveReducer from './cartActive'
+import productsReducer from './products'
+import cartReducer from './cart'
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  cartActive: cartActiveReducer,
+  products: productsReducer,
+  cart: cartReducer
+})
+
+export default rootReducer
